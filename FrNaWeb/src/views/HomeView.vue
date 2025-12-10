b028d8b1ded2f2d5e9f3fec7f31e871c4c00bfe8
Friedrich <Hans.Hahn@Student.HTW-Berlin.de>
2025-12-10 15:43:56 +0100

    login + DB

--- FrNaWeb/src/views/HomeView.vue
+++ FrNaWeb/src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-import NameList from '@/components/NameList.vue';
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
-    <NameList />
+    <div style="padding: 32px;">
+      <h1>Home</h1>
+      <p>Du bist eingeloggt.</p>
+    </div>
   </main>
 </template>
